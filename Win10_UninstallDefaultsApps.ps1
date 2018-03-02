0e6d5fe371bcd51b35a583b2ba6d5b07fefbf893
Brom19 <rom1.biot@gmail.com>
2018-03-02 10:44:28 +0100

    add ps1

---
+++ Win10_UninstallDefaultsApps.ps1
@@ -0,0 +1,59 @@
+#Désinstaller 3D Builder:
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+
+#Désinstaller Alarmes et Horloge
+Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+
+#Désinstaller Calculatrice
+Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+
+#Désinstaller Caméra
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+
+#Désinstaller Contacter le Support
+#Cette application ne peut pas être enlevé.
+
+#Désinstaller Cortana
+#Cette application ne peut pas être enlevé.
+
+#Désinstaller Obtenir Office
+Get-AppxPackage *officehub* | Remove-AppxPackage
+
+#Désinstaller Obtenir Skype
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+
+#Désinstaller Groove Musique
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+
+#Désinstaller Cartes
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+
+#Désinstaller Microsoft Edge
+#Cette application ne peut pas être enlevé.
+
+#Désinstaller Films et TV
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+
+#Désinstaller OneNote
+Get-AppxPackage *onenote* | Remove-AppxPackage
+
+#Désinstaller Contacts
+Get-AppxPackage *people* | Remove-AppxPackage
+
+#Désinstaller Store
+Get-AppxPackage *windowsstore* | Remove-AppxPackage
+
+#Désinstaller Sport
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+
+#Désinstaller Enregistreur vocal
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+
+#Désinstaller Météo
+Get-AppxPackage *bingweather* | Remove-AppxPackage
+
+#Désinstaller Commentaires sur Windows
+#Cette application ne peut pas être enlevé.
+
+#Désinstaller Xbox
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
